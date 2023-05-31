5dde6692924aa1fb52f669b7e6831e772c32ddde
Tewodros Abebe <teddyab729@gmail.com>
2023-05-31 10:09:55 +0300

    Update

--- config/db.js
+++ config/db.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
+require("dotenv").config();
 
 const connectDB = async () => {
   try {
+    if (!process.env.MONGO_URI) {
+      throw new Error("MONGO_URI environment variable is not defined");
+    }
     const conn = await mongoose.connect(process.env.MONGO_URI, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
